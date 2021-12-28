f9e2217b21c3539585f58c2acd7f6568adaf782e
DESKTOP-T3E4VKJ\kv210 <neokv210@gmail.com>
2021-12-28 16:55:20 +0300

    Add main application activities (go fly, zones map, about app).

---
+++ ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/GoFlyActivity.java
@@ -0,0 +1,14 @@
+package com.dji.importSDKDemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoFlyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_go_fly);
+    }
+}
---
+++ ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/ZonesMapActivity.java
@@ -0,0 +1,14 @@
+package com.dji.importSDKDemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ZonesMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zones_map);
+    }
+}
