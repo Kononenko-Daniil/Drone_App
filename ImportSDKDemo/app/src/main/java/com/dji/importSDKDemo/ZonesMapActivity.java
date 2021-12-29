35be3bfe9a94576647735df71055a6a535443e4f
DESKTOP-T3E4VKJ\kv210 <neokv210@gmail.com>
2021-12-29 14:49:19 +0300

    Add gogle map supporting in ZonesMapActivity java class.

--- ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/ZonesMapActivity.java
+++ ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/ZonesMapActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ZonesMapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import dji.internal.camera.La;
+
+public class ZonesMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zones_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        if(gMap == null){
+            gMap = googleMap;
+        }
     }
 }
