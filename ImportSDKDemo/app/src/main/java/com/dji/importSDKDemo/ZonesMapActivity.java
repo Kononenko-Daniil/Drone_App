95003e2010319313f3c4b1046813f2e51cd74a01
Kononenko-Daniil <60035004+Kononenko-Daniil@users.noreply.github.com>
2021-12-29 16:46:07 +0300

    Creating zones map page logic (#7)
    * Add gogle map supporting in ZonesMapActivity java class.
    
    * Add onClick method for "go back" button.

--- ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/ZonesMapActivity.java
+++ ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/ZonesMapActivity.java
@@ -2,13 +2,41 @@ package com.dji.importSDKDemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class ZonesMapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import dji.internal.camera.La;
+
+public class ZonesMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap gMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zones_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onBackClick(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        if(gMap == null){
+            gMap = googleMap;
+        }
     }
 }
