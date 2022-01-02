fdda7d80fc50f90f5e31e13c7a614b7e5f1d1a0b
DESKTOP-T3E4VKJ\kv210 <neokv210@gmail.com>
2022-01-02 19:51:45 +0300

    Optimize imports in MainActivity class.

--- ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/MainActivity.java
+++ ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/MainActivity.java
@@ -1,35 +1,10 @@
 package com.dji.importSDKDemo;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
 import android.content.Intent;
-import android.os.Handler;
-import android.os.Looper;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import dji.common.error.DJIError;
-import dji.common.error.DJISDKError;
-import dji.sdk.base.BaseComponent;
-import dji.sdk.base.BaseProduct;
-import dji.sdk.sdkmanager.DJISDKInitEvent;
-import dji.sdk.sdkmanager.DJISDKManager;
-
-import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
     private Intent intent;
