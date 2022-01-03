0459622902f2d9031dc84f459ef85f62eecca3af
DESKTOP-T3E4VKJ\kv210 <neokv210@gmail.com>
2022-01-03 16:57:18 +0300

    Add "page in development" and redirecting home button in "go fly" page.

--- ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/GoFlyActivity.java
+++ ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/GoFlyActivity.java
@@ -2,13 +2,21 @@ package com.dji.importSDKDemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GoFlyActivity extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_fly);
+    }
+
+    public void onBackClick(View view){
+        intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
