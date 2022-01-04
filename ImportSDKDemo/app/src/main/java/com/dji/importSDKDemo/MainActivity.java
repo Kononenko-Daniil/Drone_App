66d3d97656b1bb986f1bb2a33b3b91972cb128d4
DESKTOP-T3E4VKJ\kv210 <neokv210@gmail.com>
2022-01-04 16:54:12 +0300

    Transfer logic from Zones Map page to Main Page.

--- ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/MainActivity.java
+++ ImportSDKDemo/app/src/main/java/com/dji/importSDKDemo/MainActivity.java
@@ -1,18 +1,163 @@
 package com.dji.importSDKDemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.maps.android.PolyUtil;
+
+import dji.common.flightcontroller.FlightControllerState;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.products.Aircraft;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private Intent intent;
+    private GoogleMap gMap;
+    private Handler mHandler;
+    private double droneLocationLat = 181, droneLocationLng = 181;
+    private Marker droneMarker = null;
+    private FlightController mFlightController;
+    private Polygon polygon;
+    private Circle circle;
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(DJIDemoApplication.FLAG_CONNECTION_CHANGE);
+        registerReceiver(mReceiver, filter);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.zones_map);
+        mapFragment.getMapAsync(this);
+
+        mHandler = new Handler(Looper.getMainLooper());
+    }
+
+    protected BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            onProductConnectionChange();
+        }
+    };
+
+    private void onProductConnectionChange()
+    {
+        initFlightController();
+    }
+
+    private void initFlightController() {
+
+        BaseProduct product = DJIDemoApplication.getProductInstance();
+        if (product != null && product.isConnected()) {
+            if (product instanceof Aircraft) {
+                mFlightController = ((Aircraft) product).getFlightController();
+            }
+        }
+
+        if (mFlightController != null) {
+            mFlightController.setStateCallback(new FlightControllerState.Callback() {
+
+                @Override
+                public void onUpdate(FlightControllerState djiFlightControllerCurrentState) {
+                    droneLocationLat = djiFlightControllerCurrentState.getAircraftLocation().getLatitude();
+                    droneLocationLng = djiFlightControllerCurrentState.getAircraftLocation().getLongitude();
+                    updateDroneLocation();
+                }
+            });
+        }
+    }
+
+    public static boolean checkGpsCoordinates(double latitude, double longitude) {
+        return (latitude > -90 && latitude < 90 && longitude > -180 && longitude < 180) && (latitude != 0f && longitude != 0f);
+    }
+
+    private void updateDroneLocation(){
+
+        LatLng pos = new LatLng(droneLocationLat, droneLocationLng);
+        //Create MarkerOptions object
+        final MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(pos);
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.aircraft));
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (droneMarker != null) {
+                    droneMarker.remove();
+                }
+
+                if (checkGpsCoordinates(droneLocationLat, droneLocationLng)) {
+
+                    droneMarker = gMap.addMarker(markerOptions);
+                    boolean con = PolyUtil.containsLocation(new LatLng(droneLocationLat, droneLocationLng),
+                            polygon.getPoints(), false);
+                    String c = new Boolean(con).toString();
+                    if(con){
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                c, Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+            }
+        });
+    }
+
+    public void onBackClick(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap){
+        if(gMap == null){
+            gMap = googleMap;
+        }
+
+        polygon = googleMap.addPolygon(new PolygonOptions()
+                .clickable(true)
+                .add(new LatLng(-27.457, 153.040),
+                        new LatLng(-33.852, 151.211),
+                        new LatLng(-37.813, 144.962),
+                        new LatLng(-34.928, 138.599)));
+
+        circle = gMap.addCircle(new CircleOptions()
+                .center(new LatLng(52.040494, 29.251288))
+                .radius(300)
+                .strokeWidth(10)
+                .strokeColor(Color.GREEN)
+                .fillColor(Color.argb(128, 255, 0, 0)));
     }
 
     public void OnButtonCategoryClick(View view){
